a421559493cb4b48c317defd784a1da796a20a36
stian.alsos@gmail.com <stian.alsos@gmail.com>
2015-05-15 00:13:19 +0200

    Add new routes and secrets.js

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var secrets = require('secrets.js');
 
 var routes = require('./routes/index');
 var login = require('./routes/login');
-//var users = require('./routes/users');
+var game = require('./routes/game');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/verify', login);
+app.use('/game', game);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/game.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var secrets = require('secrets.js');
+var router = express.Router();
+
+router.get('/new', function(req, res, next) {
+  res.render('new');
+});
+
+router.post('/create', function(req, res, next) {
+  res.json({token: "accepted", percentage: 50});
+});
+
+router.get('/show', function(req, res, next) {
+  res.json({token: "accepted", percentage: 50});
+});
+
+router.post('/concede', function(req, res, next) {
+  res.json({token: "accepted", percentage: 50});
+});
+
+module.exports = router;
