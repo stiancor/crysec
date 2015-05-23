6e3ed7f4e7c34f4db74b110f4149f5b22ed6ebb0
stian.alsos@gmail.com <stian.alsos@gmail.com>
2015-05-23 20:27:29 +0200

    Add mongo dep and connection

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var secrets = require('secrets.js');
 
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/nodetest1');
+
 var routes = require('./routes/index');
 var login = require('./routes/login');
 var game = require('./routes/game');
