600f14d3c9360340d9a512ffaac8efc6181658b0
stian.alsos@gmail.com <stian.alsos@gmail.com>
2015-05-23 23:18:26 +0200

    Redirecting and generating key

--- routes/game.js
+++ routes/game.js
@@ -12,14 +12,20 @@ router.post('/create', function(req, res, next) {
   if (Object.keys(v.errors).length === 0) {
   	var secretHex = secrets.str2hex(v.secret);
     var shares = secrets.share(secretHex, parseInt(v.parties), parseInt(v.threshold));    
-  	res.json({token: "accepted", percentage: 50});	
+  	res.redirect('show/?id=' + secrets.random(128));	
   } else {
     res.render('game/new', v);	
   }
 });
 
+router.get('/create', function(req, res, next) {	
+  console.log('Testing');
+  res.redirect('new');
+});
+
 router.get('/show', function(req, res, next) {
-  res.json({token: "accepted", percentage: 50});
+  console.log(req.query.id)	
+  res.render('game/show', {message: req.query.id});
 });
 
 router.post('/concede', function(req, res, next) {
