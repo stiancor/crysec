8e2f4265cca45915e31e5a711cf0d6578f3d4e6a
stian.alsos@gmail.com <stian.alsos@gmail.com>
2015-05-24 20:04:07 +0200

    Add db connection to all routes

--- app.js
+++ app.js
@@ -28,6 +28,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
 app.use('/', routes);
 app.use('/verify', login);
 app.use('/game', game);
