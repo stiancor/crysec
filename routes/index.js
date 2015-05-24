8d066323a77ef8e06358bfaf86dd4d327e6cbd34
stian.alsos@gmail.com <stian.alsos@gmail.com>
2015-05-24 23:05:19 +0200

    New title, and removing wrong use of title in page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/:uuid', function(req, res, next) {
-  res.render('index', { title: 'RESTRICTED ACCESS', uuid: req.param("uuid") });
+  res.render('index', { title: 'LOG IN', uuid: req.param("uuid") });
 });
 
 module.exports = router;
