4a1a62e1e4d0ee22d2ba80987478cbd49b1e88c8
stian.alsos@gmail.com <stian.alsos@gmail.com>
2015-05-25 20:41:01 +0200

    Progress bar on login page

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/:id', function(req, res, next) {
-  res.render('index', { title: 'LOG IN', uuid: req.params.id });
+  res.render('index', { title: 'LOG IN', id: req.params.id });
 });
 
 module.exports = router;
--- routes/login.js
+++ routes/login.js
@@ -2,11 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/', function(req, res, next) {
-  var uuid = req.body.uuidCode;	
   var token = req.body.token;
-  console.log(req.body)
+  var id = req.body.uuidCode;
+  var games = req.db.get('games');
 
-  res.json({token: "accepted", percentage: 50});
+  games.findOne({ ref: req.body.uuidCode }, function(err, game) {
+  	if(err) throw err;
+  	if(game == null) {
+  		res.sendStatus(404);
+  	} else {
+  		games.findAndModify({gameId: id, tagRef: token}, {$set: {isScanned: true}}, function(err, doc) {
+  		console.log(doc);
+  			games.find({gameId: id, isScanned: true}, function(err, doc) {
+  				res.render('game/progress', {percent: ((doc.length) / game.threshold)*100})
+  			})
+  		});		
+  	}
+  });
 });
 
 module.exports = router;
