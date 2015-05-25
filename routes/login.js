fbd8a5c14fb9494fb01eeb176455ef44057c58bf
stian.alsos@gmail.com <stian.alsos@gmail.com>
2015-05-25 20:59:56 +0200

    Small cleanup

--- routes/login.js
+++ routes/login.js
@@ -2,22 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/', function(req, res, next) {
-  var token = req.body.token;
-  var id = req.body.uuidCode;
-  var games = req.db.get('games');
+  	var token = req.body.token;
+  	var id = req.body.uuidCode;
+  	var games = req.db.get('games');
 
-  games.findOne({ ref: req.body.uuidCode }, function(err, game) {
-  	if(err) throw err;
-  	if(game == null) {
-  		res.sendStatus(404);
-  	} else {
-  		games.findAndModify({gameId: id, tagRef: token}, {$set: {isScanned: true}}, function(err, doc) {
-  			games.find({gameId: id, isScanned: true}, function(err, doc) {
-  				res.render('game/progress', {percent: ((doc.length) / game.threshold)*100})
-  			})
-  		});		
-  	}
-  });
+  	games.findOne({ ref: id }, function(err, game) {
+  		if(err) throw err;
+  		if(game == null) {
+  			res.sendStatus(404);
+  		} else {
+  			games.findAndModify({gameId: id, tagRef: token}, {$set: {isScanned: true}}, function(err, doc) {
+  				games.find({gameId: id, isScanned: true}, function(err, doc) {
+  					res.render('game/progress', {percent: ((doc.length) / game.threshold)*100})
+  				})
+  			});		
+  		}
+  	});
 });
 
 module.exports = router;
