5b9d08a1a951ad4d4245ed766e41a77398adccff
stian.alsos@gmail.com <stian.alsos@gmail.com>
2015-05-25 20:51:29 +0200

    When refreshing, show current login status

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/:id', function(req, res, next) {
-  res.render('index', { title: 'LOG IN', id: req.params.id });
+  console.log(req.params.id)
+  var games = req.db.get('games');	
+  games.findOne({ ref: req.params.id }, function(err, game) {
+  	if(err) throw err;
+  	if(game != null) {
+  		games.find({gameId: req.params.id, isScanned: true}, function(err, doc) {
+  			if(err) throw err;
+  			res.render('index', { title: 'LOG IN', id: req.params.id, percent: (doc.length / game.threshold) * 100 });	
+  		});
+  	} else {
+  		res.sendStatus(404);
+  	}
+  });
 });
 
 module.exports = router;
