5a81c717304ff69f16d4fe6e03ddbec52b945b41
stian.alsos@gmail.com <stian.alsos@gmail.com>
2015-06-17 23:08:00 +0200

    Tweaking login plot

--- loginPlot.js
+++ loginPlot.js
@@ -7,14 +7,19 @@ var fakeKey = "LdVps9VrE5bU0nNvN/9iBXz  Drhp4QzaHRSqM5hqjwHgMoXV  Z43DARyIaO6NYU
 + "XVZ43DARyIaO6NYU6vM0Ovy  4I1xjtypUlhN+GhcepAFHt3Z  NxxLoRBQitt3I2MGYG/LIey  8ZbGbPZshsr7U7VBkiZkSnHg" + linebreak
 + "2HwHffRFJ7JhB6ECoywiXgY  nN5u2T0jVKVKsZyS/TfC0fqx  DFf4y2dJRakQnHwKZBocejj  bRIFg8c0ttVFkJ9HBX414JDT" + linebreak
 + "36A6+Y2Nvpp3h+WbkHLkwce  Xd6NcENOBDifrxm9j/DHJFtO  gm1ZNwAw6ltHP88nW6ygYLU  asb0N4P7MJfacgjLF/u72e6r" + linebreak
-+ "nDcgwrvIUAu6rvIUAu676rG  xLoRBQittI2MGYGfsH+ey8Zb  Krhp4QzaHRS8M5hqjwHgMoP  /efJG8+0qQH489MhkV5EKdEM" + linebreak + linebreak;
++ "4CgBkLsdIjhUMPp31iyMZ61  2UOcNUvVj/DHJFtOKW6ygYLU  4FA8psA15WZD52aGDUdF849  IsS2VHY030vJ6v9llra746tG" + linebreak
++ "XgyXxxO2pR9ALj/Tid/I/wQ  ItW1yKQKW35ybP/nNTJcFqRg  YKfgsmTgmZG1TZ9to571gYy  LHkpiRta/+rADUJoSbSrhnU2" + linebreak
++ "eK/9MBFz7b+B0hnJ+r4ocn6  p+oMyRlgi97h/ZBl9tvhfDOY  ZuFiS1SV4P7iUPOh9Pm/aku  JYs6k+nBQYgL/GHzwihkuFEr" + linebreak
++ "GxV3I2tnJXwH/FxBF7mwhIM  LhMDW7u8mF1Pn/pLdbXFUFFr  Lnx3pzya78QWt09M5rQXb0V  8qTsX6l9YHg8BdB7elROGnzU" + linebreak
++ "3M71vHhDWaxzKfwSu9XstSS  t5vRsI0hJU8h8dhCSX4lPB8n  kv3LL11rELkfk13t+fbf/lE  dF4y7WTaYFhL6r1GK+JfachQ" + linebreak
++ "QVElk0A6YN5EYxJU0Lt3to2  1v1z3OQ5kA6Z0Q1MmwJ4I17B  1kA1RjoHOxttjj970j8JN8h  tjj9m1X76Y/XsuZMxtQ5Iik4" + linebreak + linebreak;
 
-	return [['Logging on...' + linebreak + linebreak, 1000],
+	return [['<span style="color: lightblue">Logging on...</span>' + linebreak + linebreak, 1000],
 ['Fetching access rights...' + linebreak + linebreak, 2000],
 [fakeKey, 500],
-['Setting access lever <span style="color: red">COSMIC TOP SECRET</span>' + linebreak,3000],
+['Setting access lever <span style="color: red">COSMIC TOP SECRET</span>' + linebreak,2500],
 ['<span style="color: green">Login successful!</span>' + linebreak, 1500],
-['<span style="color: yellow">Decrypting secret</span>'  + linebreak, 1000],
+['<span style="color: yellow">Decrypting secret - this might take a while</span>'  + linebreak, 1000],
 ['...',250],
 ['..',180],
 ['.',400],
@@ -31,5 +36,21 @@ var fakeKey = "LdVps9VrE5bU0nNvN/9iBXz  Drhp4QzaHRSqM5hqjwHgMoXV  Z43DARyIaO6NYU
 ['..',180],
 ['.',400],
 ['.....',320],
-[linebreak + linebreak + '<span style="color: lightgreen;">Secret message was successfully decrypted - redirecting</span>',2500]]
+['.',200],
+['...',320],
+['.',420],
+['...',250],
+['..',180],
+['.',400],
+['.....',320],
+['....',400],
+['..',190],
+['...',250],
+['...',250],
+['..',180],
+['.',400],
+['.....',320],
+['....',400],
+['..',190],
+[linebreak + linebreak + '<span style="color: green;">Secret message was successfully decrypted - redirecting</span>',2500]]
 }
