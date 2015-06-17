300f19e54775a103468d3f7f26b91f4e1150066b
stian.alsos@gmail.com <stian.alsos@gmail.com>
2015-06-17 21:50:26 +0200

    Added console output when logging in

---
+++ loginPlot.js
@@ -0,0 +1,35 @@
+module.exports = function() {
+var linebreak = "<br/>"; 
+var fakeKey = "LdVps9VrE5bU0nNvN/9iBXz  Drhp4QzaHRSqM5hqjwHgMoXV  Z43DARyIaO6NYU6vM0Ovy4I  1xjtypUlhN+GhcepAFHt3ZNX" + linebreak
++ "xLoRBQittI2MGYG/LIey8Zb  GbPZshsr7U7VBkiZkSnHg2Hw  HffRFJ7JhB6ECoywiXgYnN5  u2T0jVKsZyf8sdJfsdLS/TfC" + linebreak
++ "0fqxDFf4y2dJRakQnHwKZBo  cejjbRIFg8c0ttVFkJ9HBX41  4JDT36A6+Y2Nvpp3hWbkHLk  wceXd6NcENOBDifrxm9j/DHJ" + linebreak
++ "FtOgm1ZNwAw6ltHPnW6ygYL  Uasb0N4P7MJfacgjLF/u72e6  rnDcgwrvIUAu676rGQ9iBXz  Drhp4QzaHH9RSqM5hqjwHgMo" + linebreak
++ "XVZ43DARyIaO6NYU6vM0Ovy  4I1xjtypUlhN+GhcepAFHt3Z  NxxLoRBQitt3I2MGYG/LIey  8ZbGbPZshsr7U7VBkiZkSnHg" + linebreak
++ "2HwHffRFJ7JhB6ECoywiXgY  nN5u2T0jVKVKsZyS/TfC0fqx  DFf4y2dJRakQnHwKZBocejj  bRIFg8c0ttVFkJ9HBX414JDT" + linebreak
++ "36A6+Y2Nvpp3h+WbkHLkwce  Xd6NcENOBDifrxm9j/DHJFtO  gm1ZNwAw6ltHP88nW6ygYLU  asb0N4P7MJfacgjLF/u72e6r" + linebreak
++ "nDcgwrvIUAu6rvIUAu676rG  xLoRBQittI2MGYGfsH+ey8Zb  Krhp4QzaHRS8M5hqjwHgMoP  /efJG8+0qQH489MhkV5EKdEM" + linebreak + linebreak;
+
+	return [['Logging on...' + linebreak + linebreak, 1000],
+['Fetching access rights...' + linebreak + linebreak, 2000],
+[fakeKey, 500],
+['Setting access lever <span style="color: red">COSMIC TOP SECRET</span>' + linebreak,3000],
+['<span style="color: green">Login successful!</span>' + linebreak, 1500],
+['<span style="color: yellow">Decrypting secret</span>'  + linebreak, 1000],
+['...',250],
+['..',180],
+['.',400],
+['.....',320],
+['....',400],
+['..',190],
+['...',250],
+['..',180],
+['.',400],
+['.....',320],
+['....',400],
+['..',190],
+['...',250],
+['..',180],
+['.',400],
+['.....',320],
+[linebreak + linebreak + '<span style="color: lightgreen;">Secret message was successfully decrypted - redirecting</span>',2500]]
+}
