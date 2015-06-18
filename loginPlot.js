6bfb84004200206de8b0bd32e58f0874c6bb8e7e
stian.alsos@gmail.com <stian.alsos@gmail.com>
2015-06-18 21:42:14 +0200

    Coolifying login console screen

--- loginPlot.js
+++ loginPlot.js
@@ -1,5 +1,22 @@
 module.exports = function() {
 var linebreak = "<br/>"; 
+var logo = "<pre>        CCCCCCCCCCCCCIIIIIIIIII               AAA" + linebreak               
++ "     CCC::::::::::::CI::::::::I              A:::A" + linebreak              
++ "   CC:::::::::::::::CI::::::::I             A:::::A" + linebreak             
++ "  C:::::CCCCCCCC::::CII::::::II            A:::::::A" + linebreak            
++ " C:::::C       CCCCCC  I::::I             A:::::::::A" + linebreak           
++ "C:::::C                I::::I            A:::::A:::::A" + linebreak          
++ "C:::::C                I::::I           A:::::A A:::::A" + linebreak         
++ "C:::::C                I::::I          A:::::A   A:::::A" + linebreak        
++ "C:::::C                I::::I         A:::::A     A:::::A" + linebreak       
++ "C:::::C                I::::I        A:::::AAAAAAAAA:::::A" + linebreak      
++ "C:::::C                I::::I       A:::::::::::::::::::::A" + linebreak     
++ " C:::::C       CCCCCC  I::::I      A:::::AAAAAAAAAAAAA:::::A" + linebreak    
++ "  C:::::CCCCCCCC::::CII::::::II   A:::::A             A:::::A" + linebreak   
++ "   CC:::::::::::::::CI::::::::I  A:::::A               A:::::A" + linebreak  
++ "     CCC::::::::::::CI::::::::I A:::::A                 A:::::A" + linebreak 
++ "        CCCCCCCCCCCCCIIIIIIIIIIAAAAAAA                   AAAAAAA<pre>"
+                                                                
 var fakeKey = "LdVps9VrE5bU0nNvN/9iBXz  Drhp4QzaHRSqM5hqjwHgMoXV  Z43DARyIaO6NYU6vM0Ovy4I  1xjtypUlhN+GhcepAFHt3ZNX" + linebreak
 + "xLoRBQittI2MGYG/LIey8Zb  GbPZshsr7U7VBkiZkSnHg2Hw  HffRFJ7JhB6ECoywiXgYnN5  u2T0jVKsZyf8sdJfsdLS/TfC" + linebreak
 + "0fqxDFf4y2dJRakQnHwKZBo  cejjbRIFg8c0ttVFkJ9HBX41  4JDT36A6+Y2Nvpp3hWbkHLk  wceXd6NcENOBDifrxm9j/DHJ" + linebreak
@@ -10,11 +27,10 @@ var fakeKey = "LdVps9VrE5bU0nNvN/9iBXz  Drhp4QzaHRSqM5hqjwHgMoXV  Z43DARyIaO6NYU
 + "4CgBkLsdIjhUMPp31iyMZ61  2UOcNUvVj/DHJFtOKW6ygYLU  4FA8psA15WZD52aGDUdF849  IsS2VHY030vJ6v9llra746tG" + linebreak
 + "XgyXxxO2pR9ALj/Tid/I/wQ  ItW1yKQKW35ybP/nNTJcFqRg  YKfgsmTgmZG1TZ9to571gYy  LHkpiRta/+rADUJoSbSrhnU2" + linebreak
 + "eK/9MBFz7b+B0hnJ+r4ocn6  p+oMyRlgi97h/ZBl9tvhfDOY  ZuFiS1SV4P7iUPOh9Pm/aku  JYs6k+nBQYgL/GHzwihkuFEr" + linebreak
-+ "GxV3I2tnJXwH/FxBF7mwhIM  LhMDW7u8mF1Pn/pLdbXFUFFr  Lnx3pzya78QWt09M5rQXb0V  8qTsX6l9YHg8BdB7elROGnzU" + linebreak
-+ "3M71vHhDWaxzKfwSu9XstSS  t5vRsI0hJU8h8dhCSX4lPB8n  kv3LL11rELkfk13t+fbf/lE  dF4y7WTaYFhL6r1GK+JfachQ" + linebreak
 + "QVElk0A6YN5EYxJU0Lt3to2  1v1z3OQ5kA6Z0Q1MmwJ4I17B  1kA1RjoHOxttjj970j8JN8h  tjj9m1X76Y/XsuZMxtQ5Iik4" + linebreak + linebreak;
 
-	return [['<span style="color: lightblue">Logging on...</span>' + linebreak + linebreak, 1000],
+	return [['<span style="color: lightblue">Trying to log in...</span>' + linebreak, 1000],
+	[logo, 800],
 ['Fetching access rights...' + linebreak + linebreak, 2000],
 [fakeKey, 500],
 ['Setting access level <span style="color: red">COSMIC TOP SECRET</span>' + linebreak,2500],
